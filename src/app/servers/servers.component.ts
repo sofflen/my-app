0235362d40c973abf74da35b864c99223d1f8f38
sofflen <nixxesgaming@gmail.com>
2020-04-30 17:33:49 +0300

    Property Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -18,7 +18,12 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
+  allowNewServer = false;
+
   constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
   ngOnInit(): void {
